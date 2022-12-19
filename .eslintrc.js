3c20e351a5f4f7b4fc2a5c0df77a318f3db3d4fc
tiennm <tiennguyen1293@gmail.com>
2022-12-19 14:28:35 +0700

    Setup Loto project

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'eslint-config-prettier',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  rules: {
+    'import/no-duplicates': 2,
+    'import/named': 0,
+    'max-params': [1, 4],
+  },
+}
